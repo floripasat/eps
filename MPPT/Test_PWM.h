31fcbe68dd9456df90817e26c69f392d9902bb38
Bruno Eiterer <brunoeiterer@gmail.com>
2016-12-13 14:08:52 -0200

    Add untracked MPPT code

---
+++ MPPT/Test_PWM.h
@@ -0,0 +1,14 @@
+/*
+ * Test_PWM.h
+ *
+ *  Created on: 05/07/2016
+ *      Author: Bruno Eiterer
+ */
+
+#ifndef TEST_PWM_H_
+#define TEST_PWM_H_
+
+#define PWM_DUTY_CYCLE	0.25		// absolute pwm duty cycle
+#define PWM_PERIOD	1000			// pwm period in us
+
+#endif /* TEST_PWM_H_ */
---
+++ MPPT/eps_ADC.h
@@ -0,0 +1,15 @@
+/*
+ * eps_ADC.h
+ *
+ *  Created on: 16/11/2016
+ *      Author: Bruno Eiterer
+ */
+
+#ifndef EPS_ADC_H_
+#define EPS_ADC_H_
+
+
+void measurement_ADC(void);
+
+
+#endif /* EPS_ADC_H_ */
