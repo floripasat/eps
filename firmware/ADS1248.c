345bc70b01898d9e4d17d5143705d6d63d6f5263
Unknown <brunoeiterer@gmail.com>
2017-01-31 17:24:15 -0200

    Add spi functions to make code more portable

--- firmware/ADS1248.c
+++ firmware/ADS1248.c
@@ -1,6 +1,6 @@
 #include <msp430.h>
 #include "ADS1248.h"
-
+#include "eps_spi.h"
 
 /*
  * ADS1248 initialization procedure: - send RESET command
@@ -25,14 +25,14 @@ void config_ADS1248(int positive_channel)
 	const int initialization_data[] = {SDATAC_command,WREG_command,0x03,(positive_channel << 3 | negative_channel),0x00,0x20,0x33,WREG_command + 0x0A,0x01,0x02,0x60,
 			RREG_command,0x03,NOP_command,NOP_command,NOP_command,NOP_command,RREG_command + 0x0A,0x01,NOP_command,NOP_command};
 
-	UCB1TXBUF = RESET_command;                     		    // Send reset command to ensure device is properly powered on
+	spi_send(RESET_command);                     		    // Send reset command to ensure device is properly powered on
 	__delay_cycles(4800);									// delay after reset of 0.6ms
 
 	chip_select_port &= ~chip_select_pin;					// pull chip select low to start communication
 
 	for(w=0;w < 21;w++)										// send all initialization commands/data
 	{
-		UCB1TXBUF = initialization_data[w];
+		spi_send(initialization_data[w]);
 		for (i = 500; i; i--);                  // Add time between transmissions to make sure slave can keep up
 	}
 
@@ -49,38 +49,38 @@ long read_ADS1248(int channel)
 	/* configure positive input channel to read */
 
 	chip_select_port &= ~chip_select_pin;		// pull chip select low to start communication
-	UCB1TXBUF = SDATAC_command;					// send stop read data continuous mode command
+	spi_send(SDATAC_command);					// send stop read data continuous mode command
 	for (i = 500; i; i--);						// Add time between transmissions to make sure slave can keep up
-	UCB1TXBUF = WREG_command;					// send write to register command
+	spi_send(WREG_command);					// send write to register command
 	for (i = 500; i; i--);						// Add time between transmissions to make sure slave can keep up
-	UCB1TXBUF = 0x00;							// write 1 data byte to 1 register
+	spi_send(0x00);							// write 1 data byte to 1 register
 	for (i = 500; i; i--);						// Add time between transmissions to make sure slave can keep up
-	UCB1TXBUF = (channel << 3) | 0x01;			// write positive input channel value (channel << 3) and negative input channel value (0x01)
+	spi_send((channel << 3) | 0x01);			// write positive input channel value (channel << 3) and negative input channel value (0x01)
 	for (i = 500; i; i--);						// Add time between transmissions to make sure slave can keep up
 
 	/* start conversion and read outputs */
 
-	UCB1TXBUF = SYNC_command;			// send command to start conversion
+	spi_send(SYNC_command);			// send command to start conversion
 	__delay_cycles(7);					// delay of 7 tclk
 	chip_select_port |= chip_select_pin;						// pull chip select high after communication is done
 
 	__delay_cycles(250000);				// dealy to wait the conversion
 	chip_select_port &= ~chip_select_pin;						// pull chips select low to start communication
 	__delay_cycles(7);					// delay of 7 tclk
-	UCB1TXBUF = RDATA_command;			// send command to read conversion result
+	spi_send(RDATA_command);			// send command to read conversion result
 	for (i = 500; i; i--);              // Add time between transmissions to make sure slave can keep up
 
-	UCB1TXBUF = NOP_command;			// send dummy command to send clock and read conversion results
+	spi_send(NOP_command);			// send dummy command to send clock and read conversion results
 	for (i = 500; i; i--);              // Add time between transmissions to make sure slave can keep up
-	temp = (long)UCB1RXBUF << 16;				// save output to variable
+	temp = (long)spi_read() << 16;				// save output to variable
 
-	UCB1TXBUF = NOP_command;			// send dummy command to send clock and read conversion results
+	spi_send(NOP_command);			// send dummy command to send clock and read conversion results
 	for (i = 500; i; i--);              // Add time between transmissions to make sure slave can keep up
-	temp |= (long)UCB1RXBUF << 8;				// save output to variable
+	temp |= (long)spi_read() << 8;				// save output to variable
 
-	UCB1TXBUF = NOP_command;			// send dummy command to send clock and read conversion results
+	spi_send(NOP_command);			// send dummy command to send clock and read conversion results
 	for (i = 500; i; i--);              // Add time between transmissions to make sure slave can keep up
-	temp |= UCB1RXBUF;				// save output to variable
+	temp |= spi_read();				// save output to variable
 
 	__delay_cycles(7);					// delay of 7 tclk
 
