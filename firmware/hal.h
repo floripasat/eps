29a6dbd452bdcf93e411d05631db9d6c75a7b16c
Unknown <brunoeiterer@gmail.com>
2017-04-20 10:53:33 -0300

    Update the 100ms timer debug pin

--- firmware/hal.h
+++ firmware/hal.h
@@ -14,8 +14,8 @@
 
 #define timer_debug_port_1s P1OUT
 #define timer_debug_pin_1s BIT0
-#define timer_debug_port_100ms P3OUT
-#define timer_debug_pin_100ms BIT0
+#define timer_debug_port_100ms P1OUT
+#define timer_debug_pin_100ms BIT1
 
 /**** ADC ****/
 
