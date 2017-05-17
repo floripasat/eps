c4af40f41e55ca533a82ae743bcc5edf52a71434
Unknown <brunoeiterer@gmail.com>
2017-05-17 15:01:12 -0300

    Add PWM 3 to debug code and fix comments

--- tests/mppt pwm/main.c
+++ tests/mppt pwm/main.c
@@ -16,10 +16,12 @@ int main(void) {
 	  P4DIR |= BIT1 | BIT2 | BIT3;   // P4.1, P4.2 and P4.3 outputs
 
 	  TBCCR0 = 16;                      		// PWM Period
-	  TBCCTL1 = OUTMOD_7;                       // CCR2 reset/set
-	  TBCCR1 = 4;                  				// CCR2 PWM duty cycle
-	  TBCCTL2 = OUTMOD_7;                       // CCR3 reset/set
-	  TBCCR2 = 4;                  				// CCR3 PWM duty cycle
+	  TBCCTL1 = OUTMOD_7;                       // CCR1 reset/set
+	  TBCCR1 = 4;                  				// CCR1 PWM duty cycle
+	  TBCCTL2 = OUTMOD_7;                       // CCR2 reset/set
+	  TBCCR2 = 4;                  				// CCR2 PWM duty cycle
+	  TBCCTL3 = OUTMOD_7;						// CCR3 reset/set
+	  TBCCR3 = 4;								// CCR3 PWM duty cycle
 
 	  while(1){
 
@@ -27,16 +29,19 @@ int main(void) {
 
 	  TBCCR1 = 8;
 	  TBCCR2 = 8;
+	  TBCCR3 = 8;
 
 	  __delay_cycles(120000000);
 
 	  TBCCR1 = 12;
 	  TBCCR2 = 12;
+	  TBCCR3 = 12;
 
 	  __delay_cycles(120000000);
 
 	  TBCCR1 = 4;
 	  TBCCR2 = 4;
+	  TBCCR3 = 4;
 
 	  }
 	
