be788c42e0135901b9be37112add84928f195c1b
Unknown <brunoeiterer@gmail.com>
2017-06-26 13:05:39 -0300

    Add energy level algorithm

---
+++ firmware/energy_level_algorithm.c
@@ -0,0 +1,21 @@
+/*
+ * energy_level_algorithm.c
+ *
+ *  Created on: 26/06/2017
+ *      Author: Bruno Eiterer
+ */
+
+
+#include "energy_level_algorithm.h"
+
+uint8_t energyLevelAlgorithm(uint16_t chargeLevel){
+
+	if(chargeLeveL >= 0.75*maxChargeLevel)
+		return 0x10;
+	else if(chargeLevel >= 0.5*maxChargeLevel)
+		return 0x20;
+	else if(chargeLevel >= 0.25*maxChargeLevel)
+		return 0x30;
+	else
+		return 0x40;
+}
