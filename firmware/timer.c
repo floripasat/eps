effc40465d70b3111d4b4270fb706d37ee10912f
brunoeiterer <brunoeiterer@gmail.com>
2017-07-21 17:27:59 -0300

    Change beacon uart baud rate to 4800kbps

--- firmware/timer.c
+++ firmware/timer.c
@@ -175,6 +175,7 @@ __interrupt void timer0_a0_isr(void){
             volatile uint8_t beacon_packet[33] = {0};
             counter_30s = 0;
 
+
             beacon_packet[0] = 0x7E;
             beacon_packet[1] = EPS_data[battery1_voltage_LSB];
             beacon_packet[2] = EPS_data[battery1_voltage_MSB];
@@ -207,7 +208,7 @@ __interrupt void timer0_a0_isr(void){
             beacon_packet[29] = EPS_data[negative_z_positive_y_panel_voltage_LSB];
             beacon_packet[30] = EPS_data[negative_z_positive_y_panel_voltage_MSB];
             beacon_packet[31] = EPS_data[eps_status];
-            beacon_packet[32] = crc8(0x03, 0x92, beacon_packet+1, 31);
+            beacon_packet[32] = crc8(0x00, 0x07, beacon_packet+1, 31);
 
             volatile uint8_t i = 0;
 
@@ -256,6 +257,8 @@ __interrupt void timer0_a0_isr(void){
 __interrupt void timer1_a0_isr(void){
 
     __enable_interrupt();
+    TA0CCTL0 &= ~CCIE;
+
 
 #ifdef _DEBUG
     timer_debug_port_100ms ^= timer_debug_pin_100ms;	// Toggle 100ms debug pin
@@ -396,6 +399,7 @@ __interrupt void timer1_a0_isr(void){
     else{
         mean_counter++;
     }
+    TA0CCTL0 = CCIE;
 }
 
 void timer_config(void){
