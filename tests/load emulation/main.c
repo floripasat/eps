1b0c1b1e405ff9dd14ca3508e7b0902bb88d9697
Unknown <brunoeiterer@gmail.com>
2017-09-26 16:16:46 -0300

    Add second transistor control

--- tests/load emulation/main.c
+++ tests/load emulation/main.c
@@ -7,12 +7,15 @@ int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
     P1DIR |= BIT3;
+    P1DIR |= BIT6;
 
     while(1){
         __delay_cycles(20*1045000);		// delay for 20 seconds
         P1OUT |= BIT3;					// set transistor base
+        P1OUT |= BIT6;					// set transistor base
         __delay_cycles(104500);			// delay for 0.1 seconds
         P1OUT &= ~BIT3;					// pull transistor base low
+        P1OUT &= ~BIT6;					// puul transistor base low
     }
 
 
