6294b315b744324ac7b94b4ab81e12f618f63b41
Unknown <brunoeiterer@gmail.com>
2017-09-26 17:58:17 -0300

    Change delays to timer

--- tests/load emulation/main.c
+++ tests/load emulation/main.c
@@ -1,23 +1,20 @@
-#include <msp430.h> 
+#include <msp430.h>
+#include "timer.h"
 
 /*
  * main.c
  */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+
+    timerConfig();
 	
+    P1DIR |= BIT4;
     P1DIR |= BIT3;
-    P1DIR |= BIT6;
 
-    while(1){
-        __delay_cycles(20*1045000);		// delay for 20 seconds
-        P1OUT |= BIT3;					// set transistor base
-        P1OUT |= BIT6;					// set transistor base
-        __delay_cycles(104500);			// delay for 0.1 seconds
-        P1OUT &= ~BIT3;					// pull transistor base low
-        P1OUT &= ~BIT6;					// puul transistor base low
-    }
+    __enable_interrupt();
 
+    while(1);
 
 	return 0;
 }
