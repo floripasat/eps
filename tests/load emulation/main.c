955acbb527883ff1bc18ef7d81defc9926f9a130
Unknown <brunoeiterer@gmail.com>
2017-09-26 15:39:54 -0300

    Add first transistor control

--- tests/load emulation/main.c
+++ tests/load emulation/main.c
@@ -6,5 +6,15 @@
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
+    P1DIR |= BIT3;
+
+    while(1){
+        __delay_cycles(20*1045000);		// delay for 20 seconds
+        P1OUT |= BIT3;					// set transistor base
+        __delay_cycles(104500);			// delay for 0.1 seconds
+        P1OUT &= ~BIT3;					// pull transistor base low
+    }
+
+
 	return 0;
 }
