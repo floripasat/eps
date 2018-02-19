5715ed4590e61dec4c636e9e0bcedb6f43e4c6cd
Unknown <brunoeiterer@gmail.com>
2018-02-19 13:54:12 -0300

    Add doxygen to ADC files

--- firmware/ADC.c
+++ firmware/ADC.c
@@ -1,8 +1,25 @@
+/*
+ ** \file
+ *
+ * \brief ADC driver source
+ *
+ * \author Bruno Vale Barbosa Eiterer <brunoeiterer@gmail.com>
+ */
+
 #include <msp430.h>
 #include "ADC.h"
 
-/*
- * main.c
+/**
+ * \brief Configures the ADC peripheral
+ *
+ * Configures all port 6 pins and pins 4, 5, 6 and 7 from port 7 as ADC inputs, allows the reference control to the ADC12 registers in the REFCTL0 register, then configures the ADC12CTL0
+ * register to turn on the ADC12, enables multiple sample and conversion, configures maximum sampling time and turns on the ADC12 internal reference. The configures the ADC12CTL1
+ * register to use pulse sample mode and to read a sequence of channels. Then configures each ADC12MCTLx register to store the result of channel x, and to use the external reference
+ * connected to +VeRef. On the last register (ADC12MCTL15), the end-of-sequence bit is also set. Then the ADC12ENC bit is set in the ADC12CTL0 register to enable the conversions.
+ *
+ * \param -
+ *
+ * \returns -
  */
 
 void adc_config(void){
@@ -10,29 +27,40 @@ void adc_config(void){
 	P6SEL |= 0xff;                             			// select port 6 function as ADC
 	P7SEL |= BIT4 + BIT5 + BIT6 + BIT7;					// select p7.4,p7.5,p7.6 and p7.7 function as ADC
 
-	REFCTL0 &= ~REFMSTR;
+	REFCTL0 &= ~REFMSTR;								// allows reference control in the ADC12 registers
 
-	ADC12CTL0 = ADC12ON + ADC12MSC + ADC12SHT0_15 + ADC12REFON; 		// turn on ADC12, enable multiple sample and conversion, extend sampling time to avoid overflow of results
-	ADC12CTL1 = ADC12SHP + ADC12CONSEQ_1;       			// use sampling timer, sequence of channels
-	ADC12MCTL0 = ADC12INCH_0 + ADC12SREF_2;             		// ref+=VeRef, channel = A0
-	ADC12MCTL1 = ADC12INCH_1 + ADC12SREF_2;              		// ref+=VeRef, channel = A1
-	ADC12MCTL2 = ADC12INCH_2 + ADC12SREF_2;              		// ref+=VeRef, channel = A2
-	ADC12MCTL3 = ADC12INCH_3 + ADC12SREF_2;        				// ref+=VeRef, channel = A3
-	ADC12MCTL4 = ADC12INCH_4 + ADC12SREF_2;        				// ref+=VeRef, channel = A4
-	ADC12MCTL5 = ADC12INCH_5 + ADC12SREF_2;        				// ref+=VeRef, channel = A5
-	ADC12MCTL6 = ADC12INCH_6 + ADC12SREF_2;        				// ref+=VeRef, channel = A6
-	ADC12MCTL7 = ADC12INCH_7 + ADC12SREF_2;        				// ref+=VeRef, channel = A7
-	ADC12MCTL8 = ADC12INCH_8 + ADC12SREF_2;        				// ref+=VeRef, channel = A8
-	ADC12MCTL9 = ADC12INCH_9 + ADC12SREF_2;        				// ref+=VeRef, channel = A9
-	ADC12MCTL10 = ADC12INCH_10 + ADC12SREF_2;        			// ref+=VeRef, channel = A10
-	ADC12MCTL11 = ADC12INCH_11 + ADC12SREF_2;        			// ref+=VeRef, channel = A11
-	ADC12MCTL12 = ADC12INCH_12 + ADC12SREF_2;        			// ref+=VeRef, channel = A12
-	ADC12MCTL13 = ADC12INCH_13 + ADC12SREF_2;        			// ref+=VeRef, channel = A13
-	ADC12MCTL14 = ADC12INCH_14 + ADC12SREF_2;        			// ref+=VeRef, channel = A14
-	ADC12MCTL15 = ADC12INCH_15 + ADC12EOS + ADC12SREF_2;   		// ref+=VeRef, channel = A15
-	ADC12CTL0 |= ADC12ENC;                    			// Enable conversions
+	ADC12CTL0 = ADC12ON + ADC12MSC + ADC12SHT0_15 + ADC12REFON; 		// turn on ADC12, enable multiple sample and conversion, extend sampling time to avoid overflow of results and turns on the internal reference
+	ADC12CTL1 = ADC12SHP + ADC12CONSEQ_1;       						// use sampling timer, sequence of channels
+	ADC12MCTL0 = ADC12INCH_0 + ADC12SREF_2;             				// ref+=VeRef, channel = A0
+	ADC12MCTL1 = ADC12INCH_1 + ADC12SREF_2;              				// ref+=VeRef, channel = A1
+	ADC12MCTL2 = ADC12INCH_2 + ADC12SREF_2;              				// ref+=VeRef, channel = A2
+	ADC12MCTL3 = ADC12INCH_3 + ADC12SREF_2;        						// ref+=VeRef, channel = A3
+	ADC12MCTL4 = ADC12INCH_4 + ADC12SREF_2;        						// ref+=VeRef, channel = A4
+	ADC12MCTL5 = ADC12INCH_5 + ADC12SREF_2;        						// ref+=VeRef, channel = A5
+	ADC12MCTL6 = ADC12INCH_6 + ADC12SREF_2;        						// ref+=VeRef, channel = A6
+	ADC12MCTL7 = ADC12INCH_7 + ADC12SREF_2;        						// ref+=VeRef, channel = A7
+	ADC12MCTL8 = ADC12INCH_8 + ADC12SREF_2;        						// ref+=VeRef, channel = A8
+	ADC12MCTL9 = ADC12INCH_9 + ADC12SREF_2;        						// ref+=VeRef, channel = A9
+	ADC12MCTL10 = ADC12INCH_10 + ADC12SREF_2;        					// ref+=VeRef, channel = A10
+	ADC12MCTL11 = ADC12INCH_11 + ADC12SREF_2;        					// ref+=VeRef, channel = A11
+	ADC12MCTL12 = ADC12INCH_12 + ADC12SREF_2;        					// ref+=VeRef, channel = A12
+	ADC12MCTL13 = ADC12INCH_13 + ADC12SREF_2;        					// ref+=VeRef, channel = A13
+	ADC12MCTL14 = ADC12INCH_14 + ADC12SREF_2;        					// ref+=VeRef, channel = A14
+	ADC12MCTL15 = ADC12INCH_15 + ADC12EOS + ADC12SREF_2;   				// ref+=VeRef, channel = A15
+	ADC12CTL0 |= ADC12ENC;                    							// Enable conversions
 
 }
+
+/**
+ * \brief Reads an ADC channel
+ *
+ * Clears the ADC12SC bit in the ADC12CTL0 and then sets it to start a conversion. Waits for the conversion of the selected channel to finish, then returns the result of the
+ * conversion.
+ *
+ * \param channel is the channel to be read
+ *
+ * \returns The result of the conversion. Returns -1 if no channel could be read.
+ */
 
 int16_t adc_read(uint8_t channel){
 
--- firmware/ADC.h
+++ firmware/ADC.h
@@ -1,8 +1,9 @@
-/*
- * eps_ADC.h
+/**
+ * \file
  *
- *  Created on: 16/11/2016
- *      Author: Bruno Eiterer
+ * \brief ADC driver header
+ *
+ * \author Bruno Vale Barbosa Eiterer <brunoeiterer@gmail.com>
  */
 
 #ifndef ADC_H_
@@ -10,7 +11,19 @@
 
 #include <stdint.h>
 
+/**
+ * \fn adc_config
+ *
+ * \brief Configures the ADC peripheral
+ */
+
 void adc_config(void);
+
+/**
+ * \fn adc_read
+ *
+ * \brief Reads an ADC channel
+ */
 
 int16_t adc_read(uint8_t channel);
 
