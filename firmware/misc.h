d7c55767461d13c9004627c4192af1c716ad6594
Henrique Daniel Hamisch <henriquedhamisch@gmail.com>
2018-07-23 17:21:36 -0300

    Include basic functions for filter calculation
    Functions for average computing, median computing and vector values updating were added

--- firmware/misc.h
+++ firmware/misc.h
@@ -1,9 +1,9 @@
 #ifndef MISC_H_
 #define MISC_H_
 
+#include <stdint.h>
 #include <stdio.h>
 #include "watchdog.h"
-
 
 #define TRUE 1
 #define FALSE 0
@@ -13,6 +13,8 @@ void sysled_toggle(void);
 void frame_to_string(unsigned char frame[], unsigned char string[], int size);
 void int_to_char(int data, char string[], int size);
 unsigned char hex_to_char(unsigned char byte);
-
+void update_vector(uint32_t *vector, uint8_t size, uint32_t new_value);
+uint32_t average(uint32_t value1, uint32_t value2);
+uint32_t median_value(uint32_t *vector, uint8_t size);
 
 #endif /* MISC_H_ */
