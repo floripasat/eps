68534e2e5b88b88891e824ac72928418bf440684
Ramon de Araujo Borba <ramonborba97@gmail.com>
2019-07-27 10:22:42 -0300

    Added config.h file
    Added a config.h file containing the maximum battery charge capacity as well as other general configurations.

---
+++ firmware/config.h
@@ -0,0 +1,38 @@
+/*
+ * config.h
+ *
+ * Copyright (C) 2019, Universidade Federal de Santa Catarina.
+ *
+ * This file is part of FloripaSat-EPS.
+ *
+ * FloripaSat-EPS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FloripaSat-EPS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FloripaSat-EPS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * \brief General configuration definitions.
+ * 
+ * \author Ramon de Araujo Borba <ramonborba97@gmail.com>
+ * 
+ * \date 27/07/2019
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define MAX_BATTERY_CHARGE 4800 //[mAh]
+
+#endif // CONFIG_H_
+
+//! \} End of config group
